5a116ebe2db2297fa485b59ffaa881d9a4d4776e
Saad Khan <skhan.bee20seecs@seecs.edu.pk>
2023-01-31 08:30:09 +0100

    loss: New impl of focal loss

--- model/loss.py
+++ model/loss.py
@@ -2,10 +2,16 @@ import torch
 import torch.nn.functional as F
 
 
-def focal_loss(inputs, targets):
-    gamma, weights = 2, torch.Tensor([10, 10]).to(targets.device)
-    return F.nll_loss(
-        (1 - F.softmax(inputs, dim=1)) ** gamma * F.log_softmax(inputs, dim=1),
-        targets.long(),
-        weight=weights
-    )
+def focal_loss(inputs, targets, alpha=0.5, gamma=2, reduction='mean'):
+    logpt = F.cross_entropy(inputs, targets.long(), reduction='none')
+    pt = torch.exp(-logpt)
+    focal_loss = (1 - pt) ** gamma * logpt
+    alpha_weight = alpha * targets + (1 - alpha) * (1 - targets)
+    focal_loss = alpha_weight * focal_loss
+
+    if reduction == 'mean':
+        return torch.mean(focal_loss)
+    elif reduction == 'sum':
+        return torch.sum(focal_loss)
+    else:
+        return focal_loss
