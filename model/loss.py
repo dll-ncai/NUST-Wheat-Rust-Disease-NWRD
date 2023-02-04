10640139929d8ccdf0ef3e5c88b874d2c229f208
Saad Khan <skhan.bee20seecs@seecs.edu.pk>
2023-02-04 13:30:07 +0100

    loss: Added dice loss

--- model/loss.py
+++ model/loss.py
@@ -15,3 +15,14 @@ def focal_loss(inputs, targets, alpha=0.5, gamma=2, reduction='mean'):
         return torch.sum(focal_loss)
     else:
         return focal_loss
+
+
+def dice_loss(inputs, targets, epsilon=1e-7):
+    targets_one_hot = torch.nn.functional.one_hot(targets.long(), num_classes=inputs.shape[1])
+    targets_one_hot = targets_one_hot.permute(0, 3, 1, 2).float()
+    inputs = F.softmax(inputs, dim=1)
+    targets_one_hot = targets_one_hot.type(inputs.type())
+    numerator = 2 * (inputs * targets_one_hot).sum(dim=(2,3))
+    denominator = inputs.sum(dim=(2,3)) + targets_one_hot.sum(dim=(2,3))
+    dice_coefficient = numerator / (denominator + epsilon)
+    return 1 - dice_coefficient.mean()
